0aea98f80c553cd8a88a5e7fabfffe4e11904df5
Github User <githubuser@users.noreply.github.com>
2016-09-03 17:37:02 -0400

    test

---
+++ test.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This is a file doc comment.
+ *
+ * @package     Test
+ * @since               1.0
+ */
+
+/**
+ * Test class title goes here.
+ *
+ * This is a description of this class.
+ *
+ * @since 1.0
+ */
+class Test {
+
+	/**
+	 * Variable description.
+	 *
+	 * @var string $var Comment with full stop.
+	 */
+	public $var;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param array $var Comment with full stop.
+	 */
+	public function __construct( $var ) {
+			$this->var = $var;
+	}
+}
